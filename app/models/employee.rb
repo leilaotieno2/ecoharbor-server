fa180d00e27058e352ae005b8857f652e4ac5da0
leilaotieno2 <leila.otieno@student.moringaschool.com>
2023-10-09 10:37:21 +0300

    adding validations to employee  model

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,10 @@
 class Employee < ApplicationRecord
-    belongs_to :department
-  end
+  belongs_to :department
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :user_name, presence: true, uniqueness: true
+  validates :password, presence: true
+  validates :employment_date, presence: true
+end
