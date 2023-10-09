9a7aeaa3802ffea41b1de039116b90b91ca13513
leilaotieno2 <leila.otieno@student.moringaschool.com>
2023-10-09 13:34:17 +0300

    updating the employees controller

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,36 @@
 class EmployeesController < ApplicationController
-end
+    before_action :authenticate_user!
+  
+    def index
+      @employees = Employee.all
+    end
+  
+    def show
+      @employee = Employee.find(params[:id])
+    end
+  
+    def new
+      @employee = Employee.new
+    end
+  
+    def create
+      @employee = Employee.new(employee_params)
+  
+      if @employee.save
+        # Handle successful employee creation, e.g., redirect to index page
+        redirect_to employees_path, notice: 'Employee was successfully created.'
+      else
+        # Handle validation errors, e.g., show the form again with error messages
+        render :new
+      end
+    end
+  
+    # Define other CRUD actions: edit, update, destroy
+  
+    private
+  
+    def employee_params
+      params.require(:employee).permit(:first_name, :last_name, :email, :phone_number, :user_name, :password, :employment_date, :department_id, :employee_role)
+    end
+  end
+  
