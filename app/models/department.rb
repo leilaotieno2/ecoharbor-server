5a4973ebe47ab2c759005f3b22ba7473712494ef
leilaotieno2 <leila.otieno@student.moringaschool.com>
2023-10-09 10:36:25 +0300

    adding validations to department  model

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,5 +1,7 @@
 class Department < ApplicationRecord
-    has_many :employees
-    has_many :assets_directories
-  end
-  
+  has_many :employees
+  has_many :assets_directories
+
+  validates :department_name, presence: true, inclusion: { in: ['IT', 'Finance', 'Operations', 'Facilities Management', 'HR', 'Procurement', 'Repairs and Maintenance', 'Marketing', 'Sales', 'Customer Service'] }
+  validates :department_code, presence: true, uniqueness: true
+end
