6f7b2ab87b5f36939c1b3a50f279e9a01a526584
leilaotieno2 <leila.otieno@student.moringaschool.com>
2023-10-12 16:11:38 +0300

    updating  the requests controller

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,2 +1,46 @@
+# app/controllers/requests_controller.rb
 class RequestsController < ApplicationController
-end
+    before_action :set_request, only: [:show, :edit, :update, :destroy]
+  
+    def index
+      @requests = Request.all
+      render json: @requests
+    end
+  
+    def show
+      render json: @request
+    end
+  
+    def create
+      @request = Request.new(request_params)
+      if @request.save
+        render json: @request, status: :created
+      else
+        render json: @request.errors, status: :unprocessable_entity
+      end
+    end
+  
+    def update
+      if @request.update(request_params)
+        render json: @request
+      else
+        render json: @request.errors, status: :unprocessable_entity
+      end
+    end
+  
+    def destroy
+      @request.destroy
+      head :no_content
+    end
+  
+    private
+  
+    def set_request
+      @request = Request.find(params[:id])
+    end
+  
+    def request_params
+      params.require(:request).permit(:request_id, :asset_id, :urgency, :quantity, :reason, :employee_id, :request_date, :approval_date, :request_status)
+    end
+  end
+  
