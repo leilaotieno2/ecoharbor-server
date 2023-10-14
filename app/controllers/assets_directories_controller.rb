91952c6dc426cf54d084fadd38dcb97a9306a3db
Edwin Mwangi Njoroge <edwin.njoroge@student.moringaschool.com>
2023-10-14 17:28:05 +0300

    Created index, show and create actions for assets

--- app/controllers/assets_directories_controller.rb
+++ app/controllers/assets_directories_controller.rb
@@ -1,2 +1,27 @@
 class AssetsDirectoriesController < ApplicationController
+    before_action :set_asset, only: [:show]
+    before_action :authenticate_procurement_admin, only: [:create, :update, :destroy]
+end
+
+def index
+    @assets = AssetDirectory.all 
+    render json: @assets 
+end
+
+def show 
+    render json:@asset 
+end
+# Create an Asset. Permit Procurement Admin only 
+def create 
+    if current_user.procurement_admin? 
+        @asset = AssetDirectory.new(asset_params)
+
+        if @asset.save
+            render json: @asset, status: : created
+        else 
+            render json: @asset.errors, status: : unprocessable_entity
+        end
+    else
+        render json: { error: "Unauthorized. Only procurement admins can create assets."}, status: :Unauthorized    
+    end
 end
